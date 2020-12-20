54516d915f26e3c83722dc616fdee53be3708b1b
Tulika1234 <71084110+Tulika1234@users.noreply.github.com>
2020-12-20 20:50:05 +0530

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,29 @@
+//Create variables here
+var dog
+var happyDog;
+var database;
+var foodS;
+var foodStock;
+function preload()
+{
+  //load images here
+  dog = loadImage ("dogImg.png");
+  dog2 = loadImage("dongImg1.png");
+  
+}
+
+function setup() {
+	createCanvas(800, 700);
+  
+}
+
+
+function draw() {  
+
+  drawSprites();
+  //add styles here
+
+}
+
+
+
